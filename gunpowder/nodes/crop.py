aae4c79ba0eebbeedd0fa133923a535970fb133d
rhoadesScholar <rhoades@g.harvard.edu>
2022-03-15 10:56:43 -0400

    added absolute cropping to crop node

--- gunpowder/nodes/crop.py
+++ gunpowder/nodes/crop.py
@@ -27,6 +27,14 @@ class Crop(BatchFilter):
         fraction_positive (``tuple`` of ``float``):
 
             Relative crop starting from the positive end of the provided ROI.
+
+        abs_negative (``tuple`` of ``int`` or :class:`Coordinate`):
+
+            Number of pixels to crop starting from the negative end of the provided ROI.
+
+        abs_positive (``tuple`` of ``int`` or :class:`Coordinate`):
+
+            Number of pixels to crop starting from the positive end of the provided ROI.
     '''
 
     def __init__(
@@ -34,27 +42,36 @@ class Crop(BatchFilter):
             key,
             roi=None,
             fraction_negative=None,
-            fraction_positive=None):
+            fraction_positive=None,
+            abs_negative=None,
+            abs_positive=None):
 
         if roi is not None and (
                 fraction_positive is not None or
-                fraction_negative is not None):
+                fraction_negative is not None or
+                abs_positive is not None or
+                abs_negative is not None):
             raise RuntimeError(
                 "'roi' and 'fraction_...' arguments can not be given together")
-
-        if (roi, fraction_positive, fraction_negative) == (None, None, None):
+        if (fraction_positive is not None and abs_positive is not None) or (fraction_negative is not None  and abs_negative is not None):
             raise RuntimeError(
-                "One of 'roi' and 'fraction_...' has to be given")
+                "'fraction_...' and 'abs_...' arguments can not be given together")
+
+        if all([var is None for var in (roi, fraction_positive, fraction_negative, abs_positive, abs_negative)]):
+            raise RuntimeError(
+                "One of 'roi', 'fraction_...', or 'abs_...' has to be given")
 
         if fraction_negative is not None and fraction_positive is None:
             fraction_positive = (0.0,)*len(fraction_negative)
-        if fraction_positive is not None and fraction_negative is None:
+        elif fraction_positive is not None and fraction_negative is None:
             fraction_negative = (0.0,)*len(fraction_positive)
 
         self.key = key
         self.roi = roi
         self.fraction_negative = fraction_negative
         self.fraction_positive = fraction_positive
+        self.abs_negative = abs_negative
+        self.abs_positive = abs_positive
 
     def setup(self):
 
@@ -69,15 +86,27 @@ class Crop(BatchFilter):
 
         else:
 
-            total_fraction = tuple(
-                n + p
-                for n, p in zip(self.fraction_negative, self.fraction_positive)
-            )
-            if max(total_fraction) >= 1:
-                raise RuntimeError("Sum of crop fractions exeeds 1")
+            if self.fraction_negative:
+                total_fraction = tuple(
+                    n + p
+                    for n, p in zip(self.fraction_negative, self.fraction_positive)
+                )
+                if max(total_fraction) >= 1:
+                    raise RuntimeError("Sum of crop fractions exeeds 1")
+            
+            if self.abs_positive is not None:
+                crop_positive = Coordinate(self.abs_positive)
+            else:
+                crop_positive = spec.roi.get_shape()*self.fraction_positive
 
-            crop_positive = spec.roi.get_shape()*self.fraction_positive
-            crop_negative = spec.roi.get_shape()*self.fraction_negative
+            if self.abs_negative is not None:
+                crop_negative = Coordinate(self.abs_negative)
+            else:
+                crop_negative = spec.roi.get_shape()*self.fraction_negative
+            
+            if any([(crop_positive + crop_negative) >= spec.roi.get_shape()]):
+                raise RuntimeError(f'Combination of requested positive crop ({crop_positive}) and requested negative crop ({crop_negative}) are larger than the provided ROI ({spec.roi.get_shape()}).')
+
             cropped_roi = spec.roi.grow(
                 -crop_positive,
                 -crop_negative)
